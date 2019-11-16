416ddd14572860be723b6620a509691dcfbf610c
Khartaz <tjaygallery@gmail.com>
2019-11-16 10:26:46 +0100

    branch chat-service

--- src/app/apiURL.js
+++ src/app/apiURL.js
@@ -1,10 +1,10 @@
 // LOCAL URL
-// export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
-// export const BASE_URL = 'http://localhost:8080/api';
+export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
+export const BASE_URL = 'http://localhost:8080/api';
 
 // REMOTE URL
-export const OAUTH2_REDIRECT_URI = 'https://stoic-neumann-df94d0.netlify.com/oauth2/redirect';
-export const BASE_URL = 'https://task-management-api.herokuapp.com/api';
+// export const OAUTH2_REDIRECT_URI = 'https://stoic-neumann-df94d0.netlify.com/oauth2/redirect';
+// export const BASE_URL = 'https://task-management-api.herokuapp.com/api';
 
 //ACCOUNT
 export const USER_SIGN_IN = BASE_URL + '/auth/login';
@@ -13,8 +13,6 @@ export const GET_USER_DATA = BASE_URL + '/users/me';
 export const UPDATE_USER_DATA = BASE_URL + '/users/me/settings';
 export const UPDATE_USER = BASE_URL + '/users/me';
 export const NEW_PASSWORD_REQUEST = BASE_URL + '/users/me/reset-password';
-
-
 
 export const GOOGLE_AUTH_URL = BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
